533ab0399cc8be8b589373bbfde5b5fe59dff1cf
Justin Chow <hrll71990@gmail.com>
2021-01-26 15:59:32 -0800

    routing to each service; productIds are global variables but may change later

--- server/index.js
+++ server/index.js
@@ -1,13 +1,52 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const axios = require('axios');
 const PORT = 3000;
+const reviews = 'http://localhost:3003';
+let productId;
 
 app.use(express.static(path.resolve('public')));
 
 app.use((req, res, next) => {
   console.log(`Carousel Proxy serving ${req.method} from ${req.path}`);
   next();
+});
+
+app.get('/api/picture-service/:id', (req, res) => {
+  axios.get(`http://localhost:3004${req.url}`)
+    .then(response => {res.send(response.data)})
+    .catch(err => res.send(err));
+});
+
+app.get('/api/reviews/:product_id', (req, res) => {
+  axios.get(`${reviews}${req.url}`)
+    .then((response) => res.send((response.data)))
+    .catch(e => console.log(e));
+});
+
+app.get('/api/reviews/:product_id/images', (req, res) => {
+  axios.get(`${reviews}${req.url}`)
+    .then((response) => res.send((response.data)))
+    .catch(e => console.log(e));
+});
+
+app.get('/api/reviews/:product_id/images/:place', (req, res) => {
+  axios.get(`${reviews}${req.url}`)
+    .then((response) => res.send((response.data)))
+    .catch(e => console.log(e));
+});
+
+app.patch('/api/reviews/:product_id/:id', (req, res) => {
+  axios.patch(`${reviews}${req.url}`)
+    .then((response) => res.send((response.data)))
+    .catch(e => console.log(e));
+})
+
+app.get(`/api/carousels/:id`, (req, res) => {
+  axios.get(`http://localhost:3002${req.url}`)
+    .then(response => {res.send(response.data)})
+    .catch(err => res.send(err));
 });
 
 app.listen(PORT, () => {
